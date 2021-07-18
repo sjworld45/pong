9266c5124a3a03a4198974b88350bc511aec11df
sjworld45 <47642292+sjworld45@users.noreply.github.com>
2021-07-18 16:10:16 +0530

    implemented dynamic caching

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 const CACHE_NAME = 'cache-v4';
+const dynamic_cache = 'dynamic-cache-v1'
 const FILES_TO_CACHE = [
   'index.html',
   'app.js',
@@ -8,10 +9,12 @@ const FILES_TO_CACHE = [
 ]
 
 self.addEventListener('install', e => e.waitUntil(
+  // add or addAll goes out to the server gets the files to be cached
   caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE))));
 
 // activate event
 self.addEventListener('activate', e => {
+  // waitUnitl waits for the callback to finish
   e.waitUntil(
       caches.keys().then(keys => {
         console.log('service worker has been activated');
@@ -25,6 +28,11 @@ self.addEventListener('activate', e => {
 // fetching from cache or server
 self.addEventListener('fetch', e => e.respondWith(
   caches.match(e.request).then((r) => {
-    return r || fetch(e.request)
+    return r || fetch(e.request).then(fetchRes => {
+      return caches.open(dynamic_cache).then(cache => {
+        cache.put(e.request.url, fetchRes.clone());
+        return fetchRes;
+      })
+    })
   })
 ));
